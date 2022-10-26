54d1bfaf69ed0b128f9eb1311aa642724e179f1e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2022-10-26 18:52:10 -0400

    initial upload (#1)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# redirects-website
+# Url Redirects with GitHub
+
+_Counterpart to the [redirects repo](../../../redirects)._
+_See there for more information._
---
+++ redirect.js
@@ -0,0 +1,33 @@
+// GENERATED AUTOMATICALLY, DO NOT EDIT
+let list = "W10=";
+
+list = JSON.parse(window.atob(list));
+
+// name of this repo
+const repo = "redirects-website";
+
+// normalize path. lower case, remove leading slashes, remove repo name.
+const from = window.location.pathname
+  .toLowerCase()
+  .replace(new RegExp(`^/*${repo}`), "")
+  .replace(/^(\/+)/, "");
+
+// find corresponding "to" field
+const to = list.find((entry) => entry?.from === from)?.to;
+
+// enable "preserve log" in browser dev tools to still see this after redirect
+console.info({ from, to });
+
+// immediately navigate to destination
+if (to) window.location.href = to;
+
+// if no matching destination, do a fallback action of your choice
+
+// show html error message
+const message = `
+  No redirect link found for ${from}.<br>
+`;
+window.onload = () => (document.body.innerHTML = message);
+
+// immediately navigate to some fallback url
+// window.location.href = "some-fallback-url.com/";
